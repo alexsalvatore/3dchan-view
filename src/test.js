32298fd4106ce83c82352a0b33e82d14e69e32e0
Alex <alexandre.salvatore@hardis.fr>
2022-01-05 14:32:29 +0100

    canvas view is working

--- src/test.js
+++ src/test.js
@@ -1,1 +1,17 @@
-alert("test working!")
+import { Engine, Scene } from "babylonjs";
+import { onSceneReady } from './index';
+
+const canvas = document.getElementById("renderCanvas");
+console.log(canvas);
+
+// Display an image
+/*const ctx = canvas.getContext("2d");
+let img = new Image();
+img.onload = () => {       
+    ctx.drawImage(img,0,0);
+}
+img.src = "https://pbs.twimg.com/media/FIGI6rIWYAIFbrj?format=jpg&name=900x900";
+*/
+const engine = new Engine(canvas, true, null, false);
+const scene = new Scene(engine);
+onSceneReady("",scene, "MapId");
